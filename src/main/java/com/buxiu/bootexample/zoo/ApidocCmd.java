f73648cb0806e53d2a605b5e37ae2a11ba0088b6
bubuxiu <bubuxiu.z@qq.com>
2019-03-14 16:26:36 +0800

    更改注释

--- src/main/java/com/buxiu/bootexample/zoo/ApidocCmd.java
+++ src/main/java/com/buxiu/bootexample/zoo/ApidocCmd.java
@@ -20,7 +20,7 @@ public class ApidocCmd {
 		// 生成特定的controller接口文档， null表示生成所有接口
 		String specialname = "UserController";
 		
-		int r = Jdk8Apidoc.init(packagename, sourcedir, "bugonlineappkey"); 
+		int r = Jdk8Apidoc.init(packagename, sourcedir, "bugonlineappkey"); // bugonlineappkey需要到www.bugonline.cn上创建项目获取
 		if(r != 0) { 
 			return ;
 		}
@@ -33,7 +33,7 @@ public class ApidocCmd {
 					continue;
 				}
 			} 
-			// 生成的文档默认上传到阿里云服务器，可以通过访问www.hellodev.online进行在线访问和修改
+			// 生成的文档默认上传到阿里云服务器，可以通过访问www.bugonline.cn进行在线访问和修改
 			Jdk8Apidoc.addOneApi(controller);
 		}
 	}
