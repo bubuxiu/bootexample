22f2dae1eb217f89cec084d9c7a0a793e2e4116a
bubuxiu <bubuxiu.z@qq.com>
2019-03-14 16:22:33 +0800

    接口文档自动生成

---
+++ src/main/java/com/buxiu/bootexample/zoo/ApidocCmd.java
@@ -0,0 +1,40 @@
+package com.buxiu.bootexample.zoo;
+
+import java.util.List;
+
+import com.buxiu.hellodev.zoo.apidoc.Jdk8Apidoc;
+ 
+/**
+*
+*  Apidoc 接口文档自动生成工具 命令行版
+*
+*  @Author bubuxiu@gmail.com
+*
+**/
+public class ApidocCmd {
+	 
+	public static void main(String[] args)  {
+		String packagename = "com.buxiu.bootexample";
+		String sourcedir = "/Users/bubuxiu/git/bootexample/src/main/java";
+		
+		// 生成特定的controller接口文档， null表示生成所有接口
+		String specialname = "UserController";
+		
+		int r = Jdk8Apidoc.init(packagename, sourcedir, "bugonlineappkey"); 
+		if(r != 0) { 
+			return ;
+		}
+		
+		List<String> controllerlist = Jdk8Apidoc.getContollerList();
+		
+		for(String controller:controllerlist) {
+			if(specialname != null) {
+				if(!specialname.equals(controller)) {
+					continue;
+				}
+			} 
+			// 生成的文档默认上传到阿里云服务器，可以通过访问www.hellodev.online进行在线访问和修改
+			Jdk8Apidoc.addOneApi(controller);
+		}
+	}
+}
